5963748f90b5b7f0e6ac353ff226c49092a7d81f
Hassan Arif <hassanarif87@gmail.com>
2020-11-07 23:35:13 -0800

    Manual control, toggle and autonomous control working. gains to be tuned

--- Oroach/Oroach.ino
+++ Oroach/Oroach.ino
@@ -10,12 +10,13 @@
 #define UART_RATE 9600
 
 #define AVOIDANCE_SAT 16.0
-#define PHOTO_SAT 30.0
+#define PHOTO_SAT 25.0
 #define AVOIDANCE_GAIN 0.5
 #define PHOTO_GAIN 1
+//Manual Sat
+#define WALK_SAT 5
+#define TURN_SAT 15
 
-
-// map(x, 1, 50, 50, -100)
 Servo headServo;
 Servo rightLegServo;
 Servo leftLegServo;
@@ -38,11 +39,18 @@ enum States {
   SCAN, 
   MANUAL
   };
-
+enum ManualCmds {
+  PASS,
+  FORWARD,
+  BACK, 
+  RIGHT, 
+  LEFT,
+  TOGGLE
+  };
 struct ControlStates {
-  int walk_g = 0;
+  int walk_g = 3;
   //int walk_delay = 200;
-  int stride_ang = 30;
+  int stride_ang = 20;
   int height_ang = 25;
   float photo_g = 0.0;
   float avoidance_g = 0.0;
@@ -53,23 +61,16 @@ struct ControlStates {
 
 };
 
-
-
-//float photo_g = 0.0;
-//float avoidance_g = 0.0;
-//float turn_g = 0.0;
-
-long duration; // variable for the duration of sound wave travel
-int distance; // variable for the distance measurement
-String inString = "";
-//int stride_ang = 30;
-//int height_ang = 25;
-//uint8_t n_stride = 3;
-
 int center;
 int left;
 int right;
+long duration; // variable for the duration of sound wave travel
+int distance; // variable for the distance measurement
+String inString = "";
+
+ManualCmds serial_input = 0;
 ControlStates cs;
+
 void setup() {
   headServo.attach(headPin);
   rightLegServo.attach(rightLegPin);
@@ -83,16 +84,16 @@ void scan() {
   int d_head = 800;
   headServo.write(HEAD);
   delay(200);
-  Serial.println("Center");
+  //Serial.println("Center");
   center = get_ultrasonic_reading();
   delay(d_head);
   headServo.write(HEAD+45);
-  Serial.println("Left");
+  //Serial.println("Left");
   delay(d_head);
   left = get_ultrasonic_reading();
   delay(d_head);
   headServo.write(HEAD-45);
-  Serial.println("Right");
+  //Serial.println("Right");
   delay(d_head);
   right= get_ultrasonic_reading();
   delay(d_head);
@@ -117,30 +118,28 @@ int get_ultrasonic_reading(){
   // Calculating the distance
   distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
   //Displays the distance on the Serial Monitor
-  Serial.print("Distance: ");
-  Serial.print(distance);
-  Serial.println(" cm");
+  //Serial.print("Distance: ");
+  //Serial.print(distance);
+  //Serial.println(" cm");
   return distance;
 }
 
 void walk(int speed_g, int turn) {
-  delay_time = map(speed_g, 0, 5, 1000, 100);
-  if speed_g == 0 {
-    break;
-  } else {
-  Serial.print("n Strides");
-  Serial.println(cs.n_stride);
-  midLegServo.write(MIDLEG - cs.height_ang);
-  delay(delay_time);
-  rightLegServo.write(RIGHTLEG + cs.stride_ang - turn);
-  leftLegServo.write(LEFTLEG + cs.stride_ang + turn);
-  delay(delay_time);
-  midLegServo.write(MIDLEG + cs.height_ang);
-  delay(delay_time);
-  rightLegServo.write(RIGHTLEG - cs.stride_ang + turn);
-  leftLegServo.write(LEFTLEG - cs.stride_ang - turn);
-  delay(delay_time);
-  cs.n_stride++;
+  int delay_time = map(speed_g, 0, 5, 1000, 100);
+  if (cs.walk_g != 0) {
+    Serial.print("n Strides");
+    Serial.println(cs.n_stride);
+    midLegServo.write(MIDLEG - cs.height_ang);
+    delay(delay_time);
+    rightLegServo.write(RIGHTLEG + cs.stride_ang - turn);
+    leftLegServo.write(LEFTLEG + cs.stride_ang + turn);
+    delay(delay_time);
+    midLegServo.write(MIDLEG + cs.height_ang);
+    delay(delay_time);
+    rightLegServo.write(RIGHTLEG - cs.stride_ang + turn);
+    leftLegServo.write(LEFTLEG - cs.stride_ang - turn);
+    delay(delay_time);
+    cs.n_stride++;
   }
 }
 
@@ -149,8 +148,6 @@ void get_light_diff() {
   float photores_right =25.0/173.0 * analogRead(right_photoresistor_pin);
   float photores_left = 25.0/231.0 * analogRead(left_photoresistor_pin);
   cs.photo_g = photores_right - photores_left;
-  Serial.print("Light diff: ");
-  Serial.println(cs.photo_g);
 }
 
 void update_avoidance(){
@@ -176,76 +173,90 @@ void update_turn_gains(){
   } else  {
     cs.turn_g = cs.avoidance_g + cs.photo_g;
   }
-  Serial.print("Turn gain: ");
-  Serial.println(cs.turn_g);
 }
 
 void state_machine(){
-  Serial.println("Entering StateMachine");
   switch (cs.state) {
     case IDL:
-      Serial.println("Idl");
+      //Serial.println("Idl");
       stand_stance();
       cs.state = AUTONOMOUS;
       cs.n_stride = 0;
       break;
     case SCAN:
-      Serial.println("Scan");
+      //Serial.println("Scan");
       update_avoidance();
+      update_turn_gains();
       cs.state = AUTONOMOUS;
       cs.n_stride = 0;
       break; 
+    case MANUAL:
+      //Serial.println("Manual");
+      manual_control();
+      walk(cs.walk_g, cs.turn_g);
+      break;
     case AUTONOMOUS:
-      Serial.println("Auto");
-      walk(cs.walk_delay, cs.turn_g);
-      Serial.print("Front Scan");
+      //Serial.println("Auto");
+      get_light_diff();
+      update_turn_gains();
+      walk(cs.walk_g, cs.turn_g);
+      //Serial.print("Front Scan");
       int forward_dis = get_ultrasonic_reading();
-      Serial.println(forward_dis);
+      //Serial.println(forward_dis);
       if (forward_dis < 15 || cs.n_stride_cycle <= cs.n_stride) {
         // GOTO Avoidance 
         cs.state = SCAN;
       }
       break; 
-    case MANUAL:
-      Serial.println("Manual");
-      // statements
-      void manual_control(){
 
-      break;
     default:
       Serial.println("Default");
     // statements
       break;
   }
-  Serial.println("Exiting StateMachine");
-
 }
 
+void check_toggle(){
+  if (serial_input == TOGGLE){
+    serial_input = 0;
+    if (cs.state !=MANUAL){
+      cs.state = MANUAL;
+      cs.walk_g = 0;
+      cs.turn_g = 0;
+    }
+    else{
+      cs.state = IDL;
+      cs.walk_g = 3;
+    }
+  }
+}
 void manual_control(){
-
 // 0 No new input
-// 1 toggle
-// 2 forward
+// 1 forward
+// 2 back
 // 3 right
 // 4 left
-// 5 back
   if (cs.state == MANUAL){
-
-    switch (serial_inputs) {
+    switch (serial_input) {
       case FORWARD:
-        //do something when var equals 1
+        cs.walk_g+=1;
+        cs.walk_g = min(cs.walk_g, WALK_SAT);
+        serial_input = 0;
         break;
       case BACK:
-        //do something when var equals 2
+        cs.walk_g-=1;
+        cs.walk_g = max(cs.walk_g, 0);
+        serial_input = 0;
         break;
       case RIGHT:
-        //do something when var equals 2
+        cs.turn_g+=10;
+        cs.turn_g = min(cs.turn_g, TURN_SAT);
+        serial_input = 0;
         break;
       case LEFT:
-        //do something when var equals 2
-        break;
-      case TOGGLE:
-        cd.state = IDL;
+        cs.turn_g-=10;
+        cs.turn_g = min(cs.turn_g, -TURN_SAT);
+        serial_input = 0;
         break;
       default:
         // if nothing else matches, do the default
@@ -253,11 +264,10 @@ void manual_control(){
         break;
     }
   } else {
-    cd.state = IDL;
+    cs.state = IDL;
   }
 }
 void loop() {
-
   while (Serial.available() > 0) {
     int inChar = Serial.read();
     if (isDigit(inChar)) {
@@ -266,17 +276,17 @@ void loop() {
     }
     // if you get a newline, print the string, then the string's value:
     if (inChar == '\n') {
-      Serial.print("Value:");
-      Serial.println(inString.toInt());
-      cs.state = inString.toInt();
+      //Serial.print("Value:");
+      //Serial.println(inString.toInt());
+      serial_input = inString.toInt();
     }
   }
-  delay(100);
-  Serial.print("Current State: ");
-  Serial.println(cs.state);
-
+  delay(10);
+  check_toggle();
   state_machine();
-  delay(100);
-  get_light_diff();
-  update_turn_gains();
+  delay(10);
+  Serial.print("Walk gain: ");
+  Serial.print(cs.walk_g);
+  Serial.print(" Turn gain: ");
+  Serial.println(cs.turn_g);
 }
